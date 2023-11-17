6b24adf6a82e301c18f6d4f1487a8ae10ebb583c
Mikkel Llaven ALonso <mikkel_03@outlook.com>
2023-11-17 02:14:49 -0600

    Index en Laravel

---
+++ seterWeb/resources/views/plantillas/footer.blade.php
@@ -0,0 +1,27 @@
+<footer>
+    <div class="footerSection">
+        <section class="seter">
+            <img src="img/LogoSeterBlanco.png" alt="Logo SETER" width="100" height="70">
+            <section>
+                <h3>Industrial Seter S.A de C.V.</h3>
+                <p>If you need it, we will get it</p>
+            </section> 
+        </section>
+
+        <section class="contactUs">
+            <h3>Medios de contacto:</h3>
+            <ul>
+                <li> <span class="fa-regular fa-phone"></span><a href="tel:+525516430033" target="none">+52 (55) 1643-0033</a></li>
+            </ul>  
+        </section>
+
+        <section class="aboutus">
+            <h3>About us:</h3>
+            <ul>
+                <li><a href="aboutUs.html">Misión y Visión</a></li>
+                <li><a href="aboutUs.html">Politicas de calidad</a></li>
+                <li><a href="aboutUs.html">Valores</a></li>
+            </ul>
+        </section>
+    </div>
+</footer>
---
+++ seterWeb/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+//URL Index
+Route::view('/', 'index')->name('index');
+
+/* Route::get('/', function () {
+    return view('welcome');
+});
+*/
